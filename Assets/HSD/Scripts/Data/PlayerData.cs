f0053328fc5c71969464878b00406484ba6f3fc0
HSD06040 <tinto0604@gmail.com>
2025-07-22 17:09:22 +0900

    [Feat] 저장할 플레이어 데이터 생성

---
+++ Assets/HSD/Scripts/Data/PlayerData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PlayerData
+{
+    public string Name;
+    public int Win;
+    public int Lose;
+}
