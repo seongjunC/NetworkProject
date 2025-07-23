0c85e5b9c3e9e6a06018f0c043dce722cc1b1a99
KyuSSeo <kyuri2665@gmail.com>
2025-07-23 12:31:02 +0900

    [Feat] Player Map Test
    플레이어 이동, 발사 , 맵 파괴 테스트
    
    프로젝트 세팅에서 상호작용 변경
    
    플레이어, Land,

--- Assets/Workspace/MSK/MSK Scripts/PlayerController.cs
+++ Assets/Workspace/MSK/MSK Scripts/PlayerController.cs
@@ -1,28 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     [SerializeField] Rigidbody2D _rigidbody;
     [SerializeField] float _speed = 2f;
     [SerializeField] float _jumpSpeed = 5f;
-    [SerializeField] Transform _tank;
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _rigidbody.velocity += new Vector2(0, _jumpSpeed);
-        }
-    }
+    [SerializeField] Transform player;
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         if (horizontal != 0)
         {
-            _tank.localScale = new Vector3(-horizontal, 1f, 1f);
+            player.localScale = new Vector3(-horizontal, 1f,1f);
             Vector2 velocity = _rigidbody.velocity;
             velocity.x = horizontal * _speed;
             _rigidbody.velocity = velocity;
         }
+
     }
 }
---
+++ Assets/Workspace/MSK/MSK Scripts/Throwing.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform firePivot;       // 회전할 포신 부분 (예: FirePivot)
+    [SerializeField] Transform firePoint;       // 실제 폭탄이 나갈 위치
+    [SerializeField] Bomb bombPrefab;
+
+    [Header("Controls")]
+    [SerializeField] float angle = 45f;         // 현재 각도
+    [SerializeField] float angleStep = 1f;      // 각도 변화량
+    [SerializeField] float power = 10f;         // 폭탄 발사 속도
+
+    private void Update()
+    {
+        // 각도 조절 (Up/Down)
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            angle += angleStep;
+            if (angle > 90f) angle = 90f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            angle -= angleStep;
+            if (angle < 0f) angle = 0f;
+        }
+
+        // 포신 회전 적용 (localRotation 이용)
+        firePivot.localRotation = Quaternion.Euler(0, 0, angle);
+
+        // 발사 (스페이스)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        // 디버그: 발사 방향 그리기
+        Debug.DrawRay(firePoint.position, firePoint.up * 2f, Color.red);
+    }
+
+    void Shoot()
+    {
+        Bomb newBomb = Instantiate(bombPrefab, firePoint.position, Quaternion.identity);
+        newBomb.SetVelocity(firePoint.up * power);
+    }
+}
