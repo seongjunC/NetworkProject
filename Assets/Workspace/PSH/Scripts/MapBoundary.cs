0352f0c9a0093320202adc2c5f5ff1a378844514
xmasoo <xmasoo73@gmail.com>
2025-07-24 11:38:00 +0900

    [Feat] 투사체가 맵 밖으로 벗어나면 삭제
    투사체에 태그 추가

--- Assets/Workspace/PSH/Scripts/MapBoundary.cs
+++ Assets/Workspace/PSH/Scripts/MapBoundary.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class MapBoundary : MonoBehaviour
 {
+    public SpriteRenderer mapSpriteRenderer;
+
+    public float padding = 2f;
+
+    private BoxCollider2D boxCollider;
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+
+    }
+    private void Start()
+    {
+        DeformableTerrain terrain = FindObjectOfType<DeformableTerrain>();
+        if (terrain != null)
+        {
+            mapSpriteRenderer = terrain.GetComponent<SpriteRenderer>();
+
+            if (mapSpriteRenderer == null)
+            {
+                Debug.Log("spriterenderer 없음");
+                return;
+            }
+        }
+        else
+        {
+            Debug.Log("deformableterrian 없음");
+        }
+
+        ResizeCollider();
+    }
     private void OnTriggerExit2D(Collider2D collision)
     {
         //플레이어가 맵 밖을 나갈 경우
@@ -12,9 +42,21 @@ public class MapBoundary : MonoBehaviour
             //플레이어 낙사 판정
         }
         //투사체가 맵 밖을 나갈 경우
-        else if (collision.CompareTag("Projectile"))
+        else if (collision.CompareTag("Bullet"))
         {
             //투사체 삭제
+            Destroy(collision.gameObject);
+            Debug.Log("투사체가 맵 밖으로 나감 투사체 삭제");
         }
     }
+
+    public void ResizeCollider()
+    {
+        transform.position = mapSpriteRenderer.transform.position;
+        Vector2 mapSize = mapSpriteRenderer.bounds.size;
+        boxCollider.size = mapSize + new Vector2(padding,padding);
+        boxCollider.offset = Vector2.zero;
+        Debug.Log("맵 크기에 맞게 트리거 영역 설정 완료");
+    }
+
 }
