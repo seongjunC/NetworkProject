71c8c54c732a1df1571ad5b46415a5e881399d5a
HSD06040 <tinto0604@gmail.com>
2025-07-26 03:28:36 +0900

    [Feat] 플레이어 로비 패널 데이터 연동

---
+++ Assets/Workspace/HSD/Scripts/UI/PlayerLobbyPanel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerLobbyPanel : MonoBehaviour
+{
+    [SerializeField] TMP_Text playerName;
+    [SerializeField] TMP_Text winCount;
+    [SerializeField] TMP_Text loseCount;
+    private PlayerData data;
+
+    #region LifeCycle   
+    private void OnEnable()
+    {
+        data ??= Manager.Data.PlayerData;
+
+        UpdatePlayerName(data.Name);
+        UpdateLoseCount(data.Lose);
+        UpdateWinCount(data.Win);
+
+        data.OnNameChanged += UpdatePlayerName;
+        data.OnLoseChanged += UpdateLoseCount;
+        data.OnWinChanged += UpdateWinCount;
+    }
+
+    private void OnDisable()
+    {
+        data.OnNameChanged -= UpdatePlayerName;
+        data.OnLoseChanged -= UpdateLoseCount;
+        data.OnWinChanged -= UpdateWinCount;
+    }
+    #endregion
+
+    private void UpdatePlayerName(string _playerName)
+    {
+        playerName.text = _playerName;
+    }
+    private void UpdateWinCount(int _winCount)
+    {
+        winCount.text = _winCount.ToString();
+    }
+    private void UpdateLoseCount(int _loseCount)
+    {
+        loseCount.text = _loseCount.ToString();        
+    }
+}
