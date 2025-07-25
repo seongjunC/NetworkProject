929792b3fb19ecae5f09150af2caca862899451d
KyuSSeo <kyuri2665@gmail.com>
2025-07-25 11:12:27 +0900

    [Feat] Spawn Test
    지정해 둔 스폰 포인트 위치에 플레이어 생성

--- Assets/Workspace/MSK/MSK Scripts/BattleManager.cs
+++ Assets/Workspace/MSK/MSK Scripts/BattleManager.cs
@@ -29,17 +29,17 @@ public class TestBattleManager : MonoBehaviourPun
     }
     private void PlayerSpawn()
     {
-        if (!PhotonNetwork.IsMasterClient) return;
+        if (!PhotonNetwork.IsMasterClient)
+            return;
 
         Photon.Realtime.Player[] players = PhotonNetwork.PlayerList;
 
         for (int i = 0; i < players.Length; i++)
         {
-            if (i >= _spawnPoint.Count) break;
+            if (i >= _spawnPoint.Count)
+                break;
 
             Vector3 spawnPos = _spawnPoint[i].position;
-
-            // 소유자를 명시하기 위해 커스텀 RPC로 생성 요청
             photonView.RPC("RPC_SpawnTank", players[i], spawnPos);
         }
     }
