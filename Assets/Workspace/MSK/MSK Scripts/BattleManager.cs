f62432ea3d7d597de1f02a82fd83e140d3e98f18
KyuSSeo <kyuri2665@gmail.com>
2025-07-25 11:00:42 +0900

    [Feat] Test Lobby
    테스트용 로비 작성

--- Assets/Workspace/MSK/MSK Scripts/BattleManager.cs
+++ Assets/Workspace/MSK/MSK Scripts/BattleManager.cs
@@ -7,21 +7,46 @@ using UnityEngine.UI;
 
 public class TestBattleManager : MonoBehaviourPun
 {
+    [SerializeField] private List<Transform> _spawnPoint;
     [SerializeField] Button _turnEndButton;
     private PlayerController _playerController;
+    private TestNetworkManager _networkManager;
 
     private void Start()
     {
         _turnEndButton.onClick.AddListener(TestTurnEnd);
+        PlayerSpawn();
     }
     private void TestTurnEnd()
     {
         if (_playerController != null)
             _playerController.ResetTurn();
-       // photonView.RPC("RPC_TurnFinished", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.ActorNumber);
+        photonView.RPC("RPC_TurnFinished", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.ActorNumber);
     }
     public void RegisterPlayer(PlayerController playerController)
     {
         _playerController = playerController;
+    }
+    private void PlayerSpawn()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        Photon.Realtime.Player[] players = PhotonNetwork.PlayerList;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (i >= _spawnPoint.Count) break;
+
+            Vector3 spawnPos = _spawnPoint[i].position;
+
+            // 소유자를 명시하기 위해 커스텀 RPC로 생성 요청
+            photonView.RPC("RPC_SpawnTank", players[i], spawnPos);
+        }
+    }
+
+    [PunRPC]
+    private void RPC_SpawnTank(Vector3 spawnPos)
+    {
+        PhotonNetwork.Instantiate("Prefabs/Test Tank", spawnPos, Quaternion.identity);
     }
 }
