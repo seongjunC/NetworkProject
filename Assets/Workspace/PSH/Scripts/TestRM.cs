604908149e6e25895f2d684da45f6ca263bedb2c
xmasoo <xmasoo73@gmail.com>
2025-07-28 11:59:40 +0900

    [Feat] 테스트 씬 변경

---
+++ Assets/Workspace/PSH/Scripts/TestRM.cs
@@ -0,0 +1,25 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UI;
+
+public class TestRM : MonoBehaviour
+{
+    [Header("¹öÆ°")]
+    [SerializeField] private Button startButton;
+    private void Start()
+    {
+        startButton.onClick.AddListener(GameStart);
+    }
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    public void GameStart()
+    {
+        if (PhotonNetwork.IsMasterClient)
+            PhotonNetwork.LoadLevel("Test Battle");
+    }
+}
