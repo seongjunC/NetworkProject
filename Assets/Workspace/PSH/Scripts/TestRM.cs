7da40c979a928d125c56964104fe67d7d1b7cb02
xmasoo <xmasoo73@gmail.com>
2025-07-28 12:32:34 +0900

    [Feat] 테스트용 룸이 겹치는 문제 수정

--- Assets/Workspace/PSH/Scripts/TestRM.cs
+++ Assets/Workspace/PSH/Scripts/TestRM.cs
@@ -1,11 +1,12 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.UI;
 
-public class TestRM : MonoBehaviour
+public class TestRM : MonoBehaviourPunCallbacks
 {
     [Header("버튼")]
     [SerializeField] private Button startButton;
@@ -16,6 +17,18 @@ public class TestRM : MonoBehaviour
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    // 마스터 서버 접속 성공 시 호출
+    public override void OnConnectedToMaster()
+    {
+        // 타임스탬프나 GUID를 붙여서 방 이름 충돌 방지!
+        string roomName = "PSHTestRoom";
+        PhotonNetwork.JoinOrCreateRoom(
+            roomName,
+            new RoomOptions { MaxPlayers = 4 },
+            TypedLobby.Default
+        );
     }
     public void GameStart()
     {
