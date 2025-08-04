aba5b4ac0289039645778d459c22e6249b369c63
HSD06040 <tinto0604@gmail.com>
2025-08-04 18:47:49 +0900

    [Fix] 초기화 순서 수정

--- Assets/Workspace/HSD/Scripts/Backend/LoginManager.cs
+++ Assets/Workspace/HSD/Scripts/Backend/LoginManager.cs
@@ -265,16 +265,16 @@ public class LoginManager : MonoBehaviourPunCallbacks
         Manager.Database.userRef.Child(UserDataType.Connected.ToString()).OnDisconnect().SetValue(false);
         Manager.Data.Init();
 
-        yield return new WaitForSeconds(1);
-
-        PhotonNetwork.JoinLobby();
-        Manager.UI.FadeScreen.FadeOut(1);
-
         if (string.IsNullOrEmpty(Manager.Data.PlayerData.Name))
         {
             Manager.Data.TankInventoryData.AddTankEvent("C", 1);
             Manager.UI.NickNameSelectPanel.Show();
         }
+
+        yield return new WaitForSeconds(1);
+
+        PhotonNetwork.JoinLobby();
+        Manager.UI.FadeScreen.FadeOut(1);        
 
         gameObject.SetActive(false);
         lobbyPanel.SetActive(true);
