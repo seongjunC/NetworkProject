2379206d45487e0fab7b5d804a3bbcd319323e4f
xmasoo <xmasoo73@gmail.com>
2025-08-07 18:05:23 +0900

    [Feat] 플레이어 스폰 위치 랜덤

--- Assets/Workspace/PSH/Scripts/SpawnPlayer.cs
+++ Assets/Workspace/PSH/Scripts/SpawnPlayer.cs
@@ -1,52 +1,89 @@
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class SpawnPlayer : MonoBehaviour
 {
     private void Start()
     {
-        Spawn();
+        StartCoroutine(Spawn());
     }
 
-    void Spawn()
+    IEnumerator Spawn()
     {
-        PlayerSpawn();
+        yield return StartCoroutine(PlayerSpawn());
 
         MSKTurnController.Instance.photonView.RPC(nameof(MSKTurnController.Instance.RPC_NotifySpawned), RpcTarget.All);
     }
 
     /// <summary>
-    /// 플레이어 생성 -> 현재 선택중인 탱크로 데이터 세팅 ->
+    /// 플레이어를 중복되지 않는 랜덤 위치에 생성합니다.
+    /// 마스터 클라이언트가 스폰 지점을 섞어 CustomProperties에 저장하고, 모든 클라이언트는 이를 기반으로 스폰합니다.
     /// </summary>
-    private void PlayerSpawn()
+    private IEnumerator PlayerSpawn()
     {
         int spawnCount = transform.childCount;
         if (spawnCount == 0)
         {
             Debug.LogWarning("SpawnPlayer: 자식으로 SpawnPoint가 없어요!");
-            return;
+            yield break;
         }
 
-        // 0부터 spawnCount-1까지 랜덤 인덱스
-        //int index = Random.Range(0, spawnCount);
-
-        Player[] players = PhotonNetwork.PlayerList;
-        int index = System.Array.IndexOf(players, PhotonNetwork.LocalPlayer);
-
-        if (index < 0 || index >= spawnCount)
+        // 마스터 클라이언트가 스폰 지점 순서를 섞어서 CustomProperties에 저장
+        if (PhotonNetwork.IsMasterClient)
         {
-            Debug.LogError($"SpawnPlayer: 잘못된 인덱스! index={index}, spawnCount={spawnCount}");
-            return;
+            List<int> spawnIndices = new List<int>();
+            for (int i = 0; i < spawnCount; i++)
+            {
+                spawnIndices.Add(i);
+            }
+
+            // 리스트를 랜덤하게 섞음
+            var random = new System.Random();
+            spawnIndices = spawnIndices.OrderBy(x => random.Next()).ToList();
+
+            // 섞인 리스트를 CustomProperties에 저장
+            Hashtable roomProps = new Hashtable
+            {
+                { "spawnIndices", spawnIndices.ToArray() }
+            };
+            PhotonNetwork.CurrentRoom.SetCustomProperties(roomProps);
+            Debug.Log("마스터 클라이언트가 스폰 지점을 섞었습니다: " + string.Join(", ", spawnIndices));
         }
 
-        Transform chosenPoint = transform.GetChild(index);
+        // 모든 클라이언트는 CustomProperties가 설정될 때까지 대기
+        while (PhotonNetwork.CurrentRoom.CustomProperties["spawnIndices"] == null)
+        {
+            Debug.Log("스폰 지점 정보 대기 중...");
+            yield return null; // 다음 프레임까지 대기
+        }
+
+        // 저장된 스폰 지점 리스트를 가져옴
+        int[] shuffledIndices = (int[])PhotonNetwork.CurrentRoom.CustomProperties["spawnIndices"];
+
+        // 자신의 플레이어 순서(ActorNumber는 1부터 시작하므로 플레이어 리스트의 인덱스를 사용)
+        Player[] players = PhotonNetwork.PlayerList;
+        int playerIndex = System.Array.IndexOf(players, PhotonNetwork.LocalPlayer);
+
+        if (playerIndex < 0 || playerIndex >= shuffledIndices.Length)
+        {
+            Debug.LogError($"SpawnPlayer: 잘못된 플레이어 인덱스! playerIndex={playerIndex}, shuffledIndices.Length={shuffledIndices.Length}");
+            yield break;
+        }
+
+        // 섞인 리스트에서 자신의 스폰 위치 인덱스를 가져옴
+        int spawnPointIndex = shuffledIndices[playerIndex];
+        Transform chosenPoint = transform.GetChild(spawnPointIndex);
 
         TankData data = Manager.Data.TankDataController.CurrentTank;
 
         PhotonNetwork.Instantiate($"Prefabs/{data.tankName}", chosenPoint.position, chosenPoint.rotation, 0,
             new object[] { data.tankName, data.Level });
 
-        Debug.Log($"SpawnPlayer: '{chosenPoint.name}' 위치에 스폰 완료! 인덱스 = {index}");
+        Debug.Log($"SpawnPlayer: '{chosenPoint.name}' 위치에 스폰 완료! 플레이어 인덱스 = {playerIndex}, 스폰 위치 인덱스 = {spawnPointIndex}");
     }
 }
